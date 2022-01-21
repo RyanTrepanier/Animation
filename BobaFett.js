9322598e007d49bb76b734186272524e98b43225
RyanTrepanier <79428021+RyanTrepanier@users.noreply.github.com>
2022-01-20 17:50:10 -0800

    Fixed overlapping frames

--- BobaFett.js
+++ BobaFett.js
@@ -2,14 +2,19 @@ class BobaFett {
 
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./BobaFett.png"), 1.2, 0, 30, 108, 3, .15);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./BobaFett.png"), 2.8, 0, 29, 108, 3, .08);
+
+        this.x = 0;
+        this.y = 0;
+        this.speed = 125;
     };
 
     update() {
-
+        this.x += this.speed * this.game.clockTick;
+        if (this.x > 1024) this.x = 0;
     };
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 25, 25)
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y)
     };
 }
